033f519d9f914623192b8259f02edb2c5cbd5739
gitFellpz <felipe.adrilima@gmail.com>
2025-05-25 01:40:16 -0300

    atuaizando classe

--- src/main/java/br/com/projeto/ms_pagamento/model/Pagamento.java
+++ src/main/java/br/com/projeto/ms_pagamento/model/Pagamento.java
@@ -1,9 +1,7 @@
 package br.com.projeto.ms_pagamento.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -11,15 +9,29 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @Getter
 @Setter
+
+@Entity
+@Table(name = "tb_pagamento")
 public class Pagamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private BigDecimal valor;
     private String nome;
     private String numeroCartao;
     private String validade; //validade MM-AA
     private String codigoSeguranca; //codigo de segurança XXX
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
     private Status status;
+
+    @Column(nullable = false)
     private Long pedidoId;
+
+    @Column(nullable = false)
     private Long formaPagamento;
 
 }
